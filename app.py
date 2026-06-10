3403fd332e7f98c03a807d9cc0c42df324f047c3
cpennington-ammd <cpennington@amymyersmd.com>
2026-06-10 14:38:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import os
 
 app = Flask(__name__)
 
+# Simple in-memory store for pricing (persists while server is running)
+pricing_store = {}
+
 @app.route('/v1/messages', methods=['POST', 'OPTIONS'])
 def proxy():
     if request.method == 'OPTIONS':
@@ -26,6 +29,24 @@ def proxy():
     response = jsonify(resp.json())
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response, resp.status_code
+
+@app.route('/pricing', methods=['GET', 'POST', 'OPTIONS'])
+def pricing():
+    if request.method == 'OPTIONS':
+        response = app.make_default_options_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        return response
+    if request.method == 'POST':
+        pricing_store['csv'] = request.json.get('csv', '')
+        pricing_store['meta'] = request.json.get('meta', {})
+        response = jsonify({'success': True})
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    response = jsonify(pricing_store)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == '__main__':
     app.run()
